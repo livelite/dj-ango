6916509324126543b65ce2ce7eb4688995baa970
Rytis George <rytis@ubuntu.members.linode.com>
2017-02-23 16:10:43 +0000

    Server set up

--- main/settings.py
+++ main/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3aheef1go_!yzy#m#d=w6@xbkxh3afbid%j#%9&-l*!y0mq4r0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['morninggeorge.com']
 
 
 # Application definition
@@ -119,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
